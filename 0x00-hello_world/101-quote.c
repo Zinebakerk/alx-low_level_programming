d4b9642ba490e5dd5f697d53ae207a770fc58754
Zineb <zineb.akerkkaou999@gmail.com>
2023-02-16 14:09:53 -0800

    Write a c progrma that prints exactly and that peice of art is useful

---
+++ 0x00-hello_world/101-quote.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+/**
+ * main - prints exactly "and that piece of art is useful"
+ * Dora Korpar, 2015-10-19",
+ * followed by a new line, to the standard error.
+ * Return: Always 0 (Success).
+ */
+int main(void)
+{
+write(2, "and that piece of art is useful - Dora Korpar, 2015-10-19\n", 58);
+return (1);
+}
