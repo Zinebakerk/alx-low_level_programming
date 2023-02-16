4ee4ba6b9b4c1ccc26c39eb0eb9ef522fddcba89
Zineb <zineb.akerkkaou999@gmail.com>
2023-02-16 10:48:27 -0800

    write a script that prints programming is like building multiple puzzle , followed by a new line

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,6 +1,11 @@
 #include <stdio.h>
-int main(void) 
+
+/**
+ * main - prints the phrase "Programming is like building a multilingual puzzle
+ * Return: 0 if exited properly, non-zero otherwise
+ */
+int main(void)
 {
-puts("\"Programming is like building a multilingual puzzle");
-return 0;
+	puts("\"Programming is like building a multilingual puzzle");
+	return (0);
 }
