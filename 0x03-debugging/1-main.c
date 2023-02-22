a2c057ad6052fd8d374fbbc010ff89135ac82776
Zineb <zineb.akerkkaou999@gmail.com>
2023-02-22 16:54:49 +0000

    task resolved 2

--- 0x03-debugging/1-main.c
+++ 0x03-debugging/1-main.c
@@ -13,11 +13,12 @@ printf("Infinite loop incoming :(\n");
 
 i = 0;
 
- while (i < 10)
- {
+while (i < 10)
+{
 /* putchar(i);*/
- i++;
- }
+i++;
+}
+
 printf("Infinite loop avoided! \\o/\n");
 return (0);
 }
