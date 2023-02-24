bc8985a1808ebeed3a1ea6380f8561f6df557d8c
Zineb <zineb.akerkkaou999@gmail.com>
2023-02-24 02:20:41 +0000

    task added

---
+++ 0x04-more_functions_nested_loops/100-prime_factor.c
@@ -0,0 +1,23 @@
+#include <stdio.h>
+#include <math.h>
+
+/**
+* main - main block
+* Return: 0
+*/
+int main(void)
+{
+int c;
+long num = 612852475143;
+
+for (c = (int) sqrt(num); c > 2; c++)
+{
+if (num % c == 0)
+{
+printf("%d\n", c);
+break;
+}
+}
+
+return (0);
+}
