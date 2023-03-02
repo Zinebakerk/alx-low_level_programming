c159fd0a841ed85fd7a8ca16202d5edd3aa0a35c
Zineb <zineb.akerkkaou999@gmail.com>
2023-03-02 13:14:40 +0000

    task 2 resolved concatenation using n parameter

---
+++ 0x06-pointers_arrays_strings/1-strncat.c
@@ -0,0 +1,20 @@
+#include "main.h"
+/**
+ * _strncat - function that concatenates two strings
+ * @dest : char data type
+ * @src : char data type
+ * @n: integer data type
+ * Return: pointer to the resulting string desit
+ */
+char *_strncat(char *dest, char *src, int n)
+{
+int i, j;
+for (i = 0; dest[i] != '\0'; i++)
+;
+for (j = 0; j < n; j++)
+{
+dest[i] = src[j];
+i++;
+}
+return (dest);
+}
--- 0x06-pointers_arrays_strings/main.h
+++ 0x06-pointers_arrays_strings/main.h
@@ -3,6 +3,6 @@
 
 #include <stdio.h>
 char *_strcat(char *dest, char *src);
-
+char *_strncat(char *dest, char *src, int n);
 
 #endif
