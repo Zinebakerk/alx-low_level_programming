66e1bdbe23bce656000d425a0b5969e96b3c742e
Zineb <zineb.akerkkaou999@gmail.com>
2023-03-02 18:38:32 +0000

    task resolved

--- 0x06-pointers_arrays_strings/6-cap_string.c
+++ 0x06-pointers_arrays_strings/6-cap_string.c
@@ -9,17 +9,17 @@ char *cap_string(char *str)
 int i;
 for (i = 0; str[i] != '\0'; i++)
 {
-if (str[i] >= 97 && str[i] <= 122 && ((str[i-1] == 32) || (str[i-1] == 46) || (str[i-1] == 10) || (str[i-1] == 9)))
+if (str[i] >= 97 && str[i] <= 122)
 {
-if (str[i-1] == 9)
+if (str[i - 1] == 46 || str[i - 1] == 10
+|| str[i - 1] == 9 || str[i - 1] == 32)
 {
-str[i-1] = 32;
+if (str[i - 1] == 9)
+{
+str[i - 1] = 32;
 }
 str[i] = str[i] - 32;
 }
-else 
-{
-str[i] = str[i];
 }
 }
 return (str);
