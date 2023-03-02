44a81047267b3d522c069c6fa32794084e98e18a
Zineb <zineb.akerkkaou999@gmail.com>
2023-03-02 23:49:14 +0000

    magic comes trues

--- 0x06-pointers_arrays_strings/102-magic.c
+++ 0x06-pointers_arrays_strings/102-magic.c
@@ -12,7 +12,7 @@ int main(void)
   * write your line of code here...
   * Remember:
   * - you are not allowed to use a
-  * - you are not allowed to modify 
+  * - you are not allowed to modify p 
   * - only one statement
   * - you are not allowed to code anything else than this line of code
   */
