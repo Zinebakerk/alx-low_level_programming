2234c0479a08dcc6ac1b2f4e869eb515702d0e84
Zineb <zineb.akerkkaou999@gmail.com>
2023-03-02 22:57:13 +0000

    7-leet.c solved

--- 0x06-pointers_arrays_strings/7-leet.c
+++ 0x06-pointers_arrays_strings/7-leet.c
@@ -1,35 +1,21 @@
 #include "main.h"
 /**
- * leet - function that encodes a string into 1337
- * @str: char data type
- * Return: enoded result into 1337
- */
+* leet - function that encodes a string into 1337
+* @str: char data type
+* Return: enoded result into 1337
+*/
 char *leet(char *str)
 {
-int i;
+int i, j;
+char change[] = "aAeEoOtTlL";
+char le[] = "43071";
+
+i = 0;
 for (i = 0; str[i] != '\0'; i++)
 {
-if (str[i] == 65 || str[i] == 97)
-{
-str[i] = 52;
-}
-else if (str[i] == 69 || str[i] == 101)
-{
-str[i] = 51;
-}
-else if (str[i] == 79 || str[i] == 111)
-{
-str[i] = 48;
-}
-else if (str[i] == 84 || str[i] == 116)
-{
-str[i] = 55;
-}
-else if (str[i] == 76 || str[i] == 108)
-{
-str[i] = 49;
-}
+for (j = 0; change[j] != '\0'; j++)
+if (str[i] == change[j])
+str[i] = le[j / 2];
 }
 return (str);
-
 }
