cc42200fedbd26d21794d105f454d7737104e57f
Zineb <zineb.akerkkaou999@gmail.com>
2023-03-02 22:37:18 +0000

    fixed

--- 0x06-pointers_arrays_strings/6-cap_string.c
+++ 0x06-pointers_arrays_strings/6-cap_string.c
@@ -9,19 +9,19 @@ char *cap_string(char *str)
 int i;
 for (i = 0; str[i] != '\0'; i++)
 {
-if (str[i] >= 97 && str[i] <= 122)
+if (str[0] >= 97 && str[0] <= 122)
 {
-if (str[i - 1] == 46 || str[i - 1] == 10
-|| str[i - 1] == 9 || str[i - 1] == 32 || str[i - 1] == 33
-|| str[i - 1] == 39  || str[i - 1] == 40 || str[i - 1] == 41
-|| str[i - 1] == 34 || str[i - 1] == 59 || str[i - 1] == 123
-|| str[i - 1] == 125)
+str[0] -= 32;
+}	
+if (str[i] == 46 || str[i] == 10
+|| str[i] == 9 || str[i] == 32 || str[i] == 33
+|| str[i] == 39  || str[i] == 40 || str[i] == 41
+|| str[i] == 34 || str[i] == 59 || str[i] == 123
+|| str[i] == 125)
 {
-if (str[i - 1] == 9)
+if (str[i + 1] >= 97 && str[i + 1] <= 122)
 {
-str[i - 1] = 32;
-}
-str[i] = str[i] - 32;
+str[i + 1] -= 32;
 }
 }
 }
