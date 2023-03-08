825e136dace443201d2ff58e8639949ceb7482ab
Zineb <zineb.akerkkaou999@gmail.com>
2023-03-08 22:59:44 +0000

    function fixed

--- 0x08-recursion/0-puts_recursion.c
+++ 0x08-recursion/0-puts_recursion.c
@@ -1,16 +1,16 @@
 #include "main.h"
 /**
- * _puts_recursion - function that prints a string
- * followed by a new line
- * @s: char data type
- */
+* _puts_recursion - function that prints a string
+* followed by a new line
+* @s: char data type
+*/
 void _puts_recursion(char *s)
 {
-int i;
-
-for (i = 0; s[i] != '\0'; i++)
+if (*s == '\0')
 {
-_putchar(s[i]);
-}
 _putchar('\n');
+return;
+}
+_putchar(*s);
+_puts_recursion(s + 1);
 }
