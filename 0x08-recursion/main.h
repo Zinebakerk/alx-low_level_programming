a929bdd8e39e9e21a8044fe30aca775e07486834
Zineb <zineb.akerkkaou999@gmail.com>
2023-03-08 23:37:40 +0000

    task 7 added

--- 0x08-recursion/main.h
+++ 0x08-recursion/main.h
@@ -8,3 +8,6 @@ int factorial(int n);
 int _pow_recursion(int x, int y);
 int _sqrt_recursion(int n);
 int halp(int c, int i);
+int hai(int n, int d);
+int is_prime_number(int n);
+int _sqrt(int x, int i);
