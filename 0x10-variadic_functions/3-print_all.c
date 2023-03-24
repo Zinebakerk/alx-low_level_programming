bdf1880b1ab1ca12c2a7b857b4f781f19eb2c669
Zineb <zineb.akerkkaou999@gmail.com>
2023-03-24 12:31:18 +0000

    final commit

--- 0x10-variadic_functions/3-print_all.c
+++ 0x10-variadic_functions/3-print_all.c
@@ -1,78 +1,49 @@
 #include "variadic_functions.h"
-#include <stdlib.h>
+#include <stdarg.h>
 #include <stdio.h>
-
+#include <stdlib.h>
 /**
- * _printchar - print char type element from va_list
- * @list: va_list passed to function
- */
-void _printchar(va_list list)
-{
-printf("%c", va_arg(list, int));
-}
-
-/**
- * _printstr - print string element from va_list
- * @list: va_list passed to function
- */
-void _printstr(va_list list)
-{
-char *s;
-
-s = va_arg(list, char *);
-if (s == NULL)
-s = "(nil)";
-printf("%s", s);
-}
-
-/**
- * _printfloat - print float type element from va_list
- * @list: va_list passed to function
- */
-void _printfloat(va_list list)
-{
-printf("%f", va_arg(list, double));
-}
-
-/**
- * _printint - print int type element from va_list
- * @list: va_list passed to function
- */
-void _printint(va_list list)
-{
-printf("%d", va_arg(list, int));
-}
-/**
- * print_all - print anything passed if char, int, float, or string.
- * @format: string of formats to use and print
+ * print_all - prints all arguments
+ * @format: the argument specifier
  */
 void print_all(const char * const format, ...)
 {
-unsigned int i, j;
-va_list args;
-char *sep;
+va_list ap;
+char *temp;
+int i;
 
-checker storage[] = {
-{ "c", _printchar },
-{ "f", _printfloat },
-{ "s", _printstr },
-{ "i", _printint }
-};
-
-i = 0;
-sep = "";
-va_start(args, format);
-while (format != NULL && format[i / 4] != '\0')
+va_start(ap, format);
+while (format == NULL)
+printf("\n");
+return;
+while (format[i])
 {
-j = i % 4;
-if (storage[j].type[0] == format[i / 4])
+switch (format[i])
 {
-printf("%s", sep);
-storage[j].f(args);
-sep = ", ";
+case 'c':
+printf("%c", (char) va_arg(ap, int));
+break;
+case 'i':
+printf("%d", va_arg(ap, int));
+break;
+case 'f':
+printf("%f", (float) va_arg(ap, double));
+break;
+case 's':
+temp = va_arg(ap, char*);
+if (temp != NULL)
+{
+printf("%s", temp);
+break;
 }
+printf("(nil)");
+break;
+}
+if ((format[i] == 'c' || format[i] == 'i' || format[i] == 'f' ||
+format[i] == 's') && format[(i + 1)] != '\0')
+printf(", ");
 i++;
 }
+va_end(ap);
 printf("\n");
-va_end(args);
 }
