98771ca266c993041b9aca79a30750236bea7bf5
Zineb <zineb.akerkkaou999@gmail.com>
2023-04-01 00:06:52 +0000

    modified some lines in the funciton

--- 0x12-singly_linked_lists/0-print_list.c
+++ 0x12-singly_linked_lists/0-print_list.c
@@ -8,17 +8,17 @@
  */
 size_t print_list(const list_t *h)
 {
-const list_t *element;
-size_t a;
+	const list_t *element;
+	size_t a;
 
-a = 0;
-element = h;
-while (element != NULL)
-{
-printf("[%d] %s\n", element->len, element->str);
-element = element->next;
-a++;
-}
+	a = 0;
+	element = h;
+	while (element != NULL)
+	{
+		printf("[%d] %s\n", element->len, element->str);
+		element = element->next;
+		a++;
+	}
 
-return (a);
+	return (a);
 }
