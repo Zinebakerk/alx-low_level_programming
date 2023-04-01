9ab1cbbc949fbf3f278a5d39eb09f085c83c4df4
Zineb <zineb.akerkkaou999@gmail.com>
2023-04-01 00:49:01 +0000

    function that adds a new mode at the beginningof a list

--- 0x12-singly_linked_lists/2-add_node.c
+++ 0x12-singly_linked_lists/2-add_node.c
@@ -1,5 +1,7 @@
 #include "lists.h"
-/* add_node - adds a new node at the beginning  of a list_t list.
+
+/**
+ * add_node - adds a new node at the beginning  of a list_t list.
  * @head: head of the linked list.
  * @str: string to store in the list.
  * Return: actula address of the head.
