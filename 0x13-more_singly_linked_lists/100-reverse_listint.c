4d77c59654ce2d35ff0556695619e2f95f231141
Zineb <zineb.akerkkaou999@gmail.com>
2023-04-04 23:37:51 +0000

    100 and 101 functions added

---
+++ 0x13-more_singly_linked_lists/100-reverse_listint.c
@@ -0,0 +1,30 @@
+#include "lists.h"
+
+/**
+ * reverse_listint - function that  reverses listint_t 
+ * @head: pointer to pointer to the list 
+ * Return: pointer to  new reversed list
+ */
+listint_t *reverse_listint(listint_t **head)
+{
+listint_t *prevNode, *curNode;
+
+if (!(*head))
+return (NULL);
+
+prevNode = (*head);
+*head = (*head)->next;
+curNode = *head;
+
+prevNode->next = NULL; 
+
+while (*head)
+{
+*head = (*head)->next;
+curNode->next = prevNode;
+prevNode = curNode;
+curNode = (*head);
+}
+*head = prevNode;
+return (*head);
+}
---
+++ 0x13-more_singly_linked_lists/101-print_listint_safe.c
@@ -0,0 +1,50 @@
+#include "lists.h"
+#include <stdio.h>
+
+/**
+ * length - function that finds the length of the list
+ * @first: pointer to the first node
+ * @last: pointer to the last node
+ * Return: the number of nodes between first and last
+ */
+size_t length(const listint_t *first, const listint_t *last)
+{
+int counter = 0;
+const listint_t *vis = first;
+
+while (vis != last)
+{
+vis = vis->next;
+counter++;
+}
+
+return (counter + 1);
+}
+
+/**
+ * print_listint_safe -function that  prints a listint_t 
+ * @head: pointer to the list
+ * Return:  number of nodes in the list
+ */
+size_t print_listint_safe(const listint_t *head)
+{
+size_t counter = 0, previous_length = 0;
+const listint_t *first, *last;
+
+first = last = head;
+while (last && length(first, last) > previous_length)
+{
+previous_length = length(first, last);
+
+printf("[%p] %i\n", (void *)last, last->n);
+counter++;
+last = last->next;
+}
+
+if (last)
+{
+printf("-> [%p] %i\n", (void *)last, last->n);
+}
+
+return (counter);
+}
